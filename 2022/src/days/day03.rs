4877a55a91c5ccc581e17376de50fe0df04b050a
Nicholas Feldman <nick@feldman.tech>
2022-12-03 15:45:43 -0500

    Day three

---
+++ 2022/src/days/day03.rs
@@ -0,0 +1,67 @@
+fn to_priority(c: char) -> usize {
+    match c {
+        'a'..='z' => (c as usize) - ('a' as usize) + 1,
+        'A'..='Z' => (c as usize) - ('A' as usize) + 27,
+        _ => unimplemented!(),
+    }
+}
+
+fn split_compartments(string: &str) -> (&str, &str) {
+    string.split_at(string.len() / 2)
+}
+
+fn find_in_both(left: &str, right: &str) -> char {
+    left.chars().filter(|c| right.contains(*c)).collect::<Vec<char>>()[0]
+}
+
+fn find_in_three(one: &str, two: &str, three: &str) -> char {
+    one.chars().filter(|c| two.contains(*c) && three.contains(*c)).collect::<Vec<char>>()[0]
+}
+
+
+pub fn part_one(input: &str) -> u32 {
+    let mut total: u32 = 0;
+    for line in input.lines() {
+        let (left, right) = split_compartments(line);
+        total += to_priority(find_in_both(left, right)) as u32;
+    }
+
+    total
+}
+
+pub fn part_two(input: &str) -> u32 {
+    let mut total: u32 = 0;
+    let mut elf = 0;
+
+    let collected = input.lines().collect::<Vec<_>>();
+    let chunks = collected.chunks_exact(3);
+
+    for chunk in chunks {
+        let bag1 = chunk[0];
+        let bag2 = chunk[1];
+        let bag3 = chunk[2];
+        let badge = find_in_three(bag1, bag2, bag3);
+        total += to_priority(badge) as u32
+    }
+
+    total
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        use crate::read_file;
+        let input = read_file("examples", "day03.txt");
+        assert_eq!(part_one(&input), 157);
+    }
+
+#[test]
+fn test_part_two() {
+        use crate::read_file;
+        let input = read_file("examples", "day03.txt");
+        assert_eq!(part_two(&input), 70);
+    }
+}
--- 2022/src/main.rs
+++ 2022/src/main.rs
@@ -11,15 +11,22 @@ pub fn read_file(folder: &str, name: &str) -> String {
     f.expect("Failed to read file")
 }
 
+fn solve_day(day: u8) -> (u32, u32){
+    let input = &read_file("inputs", &format!("day{:02}.txt", day));
+
+    match day {
+        1 => (days::day01::part_one(input), days::day01::part_two(input)),
+        2 => (days::day02::part_one(input), days::day02::part_two(input)),
+        3 => (days::day03::part_one(input), days::day03::part_two(input)),
+        _ => unimplemented!(),
+    }
+}
+
 fn main() {
     println!("=== Day One ===");
-    println!("Part One: {}. Part Two: {}",
-        days::day01::part_one(&read_file("inputs", "day01.txt")),
-        days::day01::part_two(&read_file("inputs", "day01.txt"))
-    );
+    println!("Part One: {}. Part Two: {}", solve_day(1).0, solve_day(1).1);
     println!("=== Day Two ===");
-    println!("Part One: {}. Part Two: {}",
-        days::day02::part_one(&read_file("inputs", "day02.txt")),
-        days::day02::part_two(&read_file("inputs", "day02.txt"))
-    );
+    println!("Part One: {}. Part Two: {}", solve_day(2).0, solve_day(2).1);
+    println!("=== Day Three ===");
+    println!("Part One: {}. Part Two: {}", solve_day(3).0, solve_day(3).1);
 }
